cca10ed249346bf3387f0d1845c4ca1c772d3523
strive <2205487298@qq.com>
2025-03-05 11:06:58 +0800

    update

--- src/main/java/com/liu/BasicStructure/Sample.java
+++ src/main/java/com/liu/BasicStructure/Sample.java
@@ -16,8 +16,8 @@ public class Sample {
         BigInteger i = new BigInteger("100000000000000000214124123123");
         System.out.println(i);
         func();
+        function();
     }
-
     /**
      * Java当中有8种基本数据类型，4种整型、2种浮点类型，1种字符类型char和1种用于真值表示的boolean类型
      * Java没有任何的无符号数据类型
@@ -30,6 +30,25 @@ public class Sample {
         long c = 30; //8字节
         byte d = 40; //1字节
         System.out.println(a + b + c + d);
+    }
+
+    /**
+     * Java当中的浮点类型
+     */
+    public static void function() {
+        float f = 3.14f; //float数值有一个后缀F或f
+        double d = 3.14; //double默认无f，也可在其后加入D或d
+        System.out.println(f);
+        System.out.println(d);
+        //数值计算中若不允许有任何误差需使用BigDecimal类
+    }
+
+    /**
+     * Java当中的char类型
+     */
+    public static void function2() {
+        char c = 'a';
+        System.out.println(c);
     }
 
 }
