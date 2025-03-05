cba086f6d8ba3cf912c723ef10f295790150a6d3
strive <2205487298@qq.com>
2025-03-05 10:12:04 +0800

    update

---
+++ src/main/java/com/liu/BasicStructure/Sample.java
@@ -0,0 +1,16 @@
+package com.liu.BasicStructure;
+
+/**
+ * This is the first sample for System.out.print().
+ */
+//在java中注释有三种方式
+public class Sample {
+
+    public static void main(String[] args) {
+        System.out.println("We will not use 'Hello World'");
+    }
+
+
+
+
+}
