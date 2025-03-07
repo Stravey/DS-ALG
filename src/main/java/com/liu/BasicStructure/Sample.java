9c1f67af6fb5f61e6bcc0024d95e170fa306b1be
strive <2205487298@qq.com>
2025-03-07 21:28:53 +0800

    update

--- src/main/java/com/liu/BasicStructure/Sample.java
+++ src/main/java/com/liu/BasicStructure/Sample.java
@@ -1,7 +1,18 @@
 package com.liu.BasicStructure;
 
-public class Sample {
-    public static void main(String[] args) {
+import com.strive.Stack.ArrayStack;
 
+public class Sample {
+
+    ArrayStack stack;
+
+    public void setStack(ArrayStack stack) {
+        this.stack = stack;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
     }
 }
