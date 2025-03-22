0575dc4e8929e53eb5eab6f146eec095fef17b54
strive <2205487298@qq.com>
2025-03-22 21:56:42 +0800

    update set

---
+++ src/main/java/com/liu/Set/test.java
@@ -0,0 +1,10 @@
+package com.liu.Set;
+
+import java.util.LinkedList;
+
+public class test {
+    public static <E> void main(String[] args) {
+        LinkedList<E> list = new LinkedList<>();
+
+    }
+}
