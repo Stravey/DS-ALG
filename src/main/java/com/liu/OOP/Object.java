02adbed0f10737e166f877aef073a2dc00751e5b
strive <2205487298@qq.com>
2025-03-29 12:23:05 +0800

    update

--- src/main/java/com/liu/OOP/Object.java
+++ src/main/java/com/liu/OOP/Object.java
@@ -1,7 +1,7 @@
 package com.liu.OOP;
 
 @SuppressWarnings("all")
-public class object {
+public class Object {
 
 
     private int ax;
@@ -12,18 +12,18 @@ public class object {
     StringBuilder message = new StringBuilder();
     StringBuilder todoList = new StringBuilder("To do:\n");
 
-    public object() {
+    public Object() {
     }
     //this指示一个方法的隐式参数
     //java中 this引用等价于C++中的this指针
-    public object(int ax,int ay){
+    public Object(int ax,int ay){
         this.ax = ax;
         this.ay = ay;
     }
 
     //从java7开始 程序会检查是否有main方法 java6之前没有main方法也可以打印hello world
     public static void main(String[] args) {
-        object object = new object(1,2);
+        Object object = new Object(1,2);
 
     }
 
