f73736d49f7fd2a9ae7d9114681b6d41586dbc13
strive <2205487298@qq.com>
2025-04-25 18:47:50 +0800

    update jdbc_second mapper testmapper

--- src/main/java/com/jdbc/jdbc_second/TestMapper/TestAgentsMapper.java
+++ src/main/java/com/jdbc/jdbc_second/TestMapper/TestAgentsMapper.java
@@ -1,4 +1,24 @@
 package com.jdbc.jdbc_second.TestMapper;
 
+import com.jdbc.jdbc_second.mapper.AgentsMapper;
+import com.jdbc.jdbc_second.pojo.Agents;
+import org.junit.Test;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public class TestAgentsMapper {
+    @Test
+    public void selectAll() throws SQLException {
+        AgentsMapper mapper = new AgentsMapper();
+        List<Agents> list = mapper.selectAll();
+        for(Agents o : list) {
+            System.out.println(o.toString());
+        }
+    }
+
+    @Test
+    public void selectByCity() throws SQLException {
+
+    }
 }
--- src/main/java/com/jdbc/jdbc_second/mapper/AgentsMapper.java
+++ src/main/java/com/jdbc/jdbc_second/mapper/AgentsMapper.java
@@ -1,8 +1,10 @@
 package com.jdbc.jdbc_second.mapper;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import com.jdbc.jdbc_second.pojo.Agents;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AgentsMapper {
 
@@ -15,6 +17,37 @@ public class AgentsMapper {
         connection = DriverManager.getConnection(url, user, password);
     }
 
+    //查询全部
+    public List<Agents> selectAll() throws SQLException {
+        String sql = "select * from agents";
+        PreparedStatement ps = connection.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+        List<Agents> list = new ArrayList<Agents>();
+        while(rs.next()) {
+            Agents o = new Agents();
+            o.setAid(rs.getString("aid"));
+            o.setAname(rs.getString("aname"));
+            o.setCity(rs.getString("city"));
+            o.setPercent(rs.getInt("percent"));
+            list.add(o);
+        }
+        return list;
+    }
 
-
+    // 按城市查询
+    public List<Agents> selectByCity(String city) throws SQLException {
+        String sql = "select * from agents where city = ?";
+        PreparedStatement ps = connection.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+        List<Agents> list = new ArrayList<Agents>();
+        while(rs.next()) {
+            Agents o = new Agents();
+            o.setAid(rs.getString("aid"));
+            o.setAname(rs.getString("aname"));
+            o.setCity(rs.getString("city"));
+            o.setPercent(rs.getInt("percent"));
+            list.add(o);
+        }
+        return list;
+    }
 }
