a5e2142f027ec21fcdd75d52a015f466e3a17476
strive <2205487298@qq.com>
2025-04-29 22:02:09 +0800

    update packet

---
+++ src/main/java/com/se/packet/Demo.java
@@ -0,0 +1,14 @@
+package com.se.packet;
+
+import org.junit.Test;
+
+@SuppressWarnings("all")
+public class Demo {
+    @Test
+    public void test() {
+        int a = 10;
+        Integer b = a;
+        System.out.println(b);
+        System.out.println(a + b);
+    }
+}
