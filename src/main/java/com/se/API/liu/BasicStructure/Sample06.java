bc5a842f2261896510e0c3243dd0f6eded6d66a2
strive <2205487298@qq.com>
2025-05-01 17:43:55 +0800

    update

--- src/main/java/com/se/API/liu/BasicStructure/Sample06.java
+++
@@ -1,7 +0,0 @@
-package com.se.API.liu.BasicStructure;
-
-public class Sample06 {
-    public static void main(String[] args) {
-
-    }
-}
--- src/main/java/com/se/API/liu/Set/E.java
+++
@@ -1,7 +0,0 @@
-package com.se.API.liu.Set;
-
-public interface E {
-    E getE();
-    E setE(E e);
-    E addE(E e);
-}
